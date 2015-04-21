29acfd73d0b09498a49b8d00f73089c5478eba9f
Nancy Wood <NancyWood@36-238-198-196.dynamic-ip.hinet.net>
2015-04-22 00:04:09 +0800

    Initial Commit

---
+++ sort.v.1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sort.v.1
+//
+//  Created by Nancy Wood on 2015/4/22.
+//  Copyright (c) 2015年 Nancy Wood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
